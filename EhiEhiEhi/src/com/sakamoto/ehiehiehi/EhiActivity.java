e417d105a32900c2cc669c908470dff32f3a25d1
herbert <misterics@gmail.com>
2012-09-23 19:13:16 +0200

    first commit

---
+++ EhiEhiEhi/src/com/sakamoto/ehiehiehi/EhiActivity.java
@@ -0,0 +1,27 @@
+package com.sakamoto.ehiehiehi;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+
+public class EhiActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ehi);
+        Button but = (Button)findViewById(R.id.button);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_ehi, menu);
+        return true;
+    }
+    
+    public void playMusic(){
+//    	MediaPlayer mp = MediaPlayer.create(getApplicationContext(), )
+    }
+}
