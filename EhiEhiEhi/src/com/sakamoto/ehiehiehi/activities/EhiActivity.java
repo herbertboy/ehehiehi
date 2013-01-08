19f3d0579a885f73a7d48323c3dadaa83ff04f98
herbert <misterics@gmail.com>
2013-01-08 20:08:56 +0100

    Aggiunta icona per lanciare il file audio

--- EhiEhiEhi/src/com/sakamoto/ehiehiehi/activities/EhiActivity.java
+++ EhiEhiEhi/src/com/sakamoto/ehiehiehi/activities/EhiActivity.java
@@ -2,6 +2,7 @@ package com.sakamoto.ehiehiehi.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.sakamoto.ehiehiehi.R;
 
@@ -23,6 +25,32 @@ public class EhiActivity extends Activity implements OnClickListener {
         setContentView(R.layout.activity_ehi);
         Button but = (Button)findViewById(R.id.button);
         but.setOnClickListener(this);
+        
+        ImageButton imBtn = (ImageButton)findViewById(R.id.play_button);
+        imBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				MediaPlayer mp = null;
+				SharedPreferences sharePref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+				String selFile = sharePref.getString("selected_file", "2");
+
+				if(selFile.equals("1")) {
+					mp = MediaPlayer.create(getApplicationContext(),R.raw.baggytrousers);
+				}
+				
+				if(selFile.equals("2")) {
+					mp = MediaPlayer.create(getApplicationContext(),R.raw.ehiehiehi);
+				}
+				
+		    	mp.start();
+		    	while(mp.isPlaying()){
+		    		//donothing 
+		    	}
+		    	mp.release();
+			}
+		});
+        
         PreferenceManager.setDefaultValues(this, R.xml.preference, false);
     }
  
@@ -46,13 +74,9 @@ public class EhiActivity extends Activity implements OnClickListener {
 	}
 
 	@Override
-	public void onClick(View v) {		
-		MediaPlayer mp = MediaPlayer.create(getApplicationContext(),R.raw.ehiehiehi);
-
-    	mp.start();
-    	while(mp.isPlaying()){
-    		//donothing 
-    	}
-    	mp.release();
+	public void onClick(View v) {
+		Intent intent = new Intent(this, SettingsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
 	}
 }
